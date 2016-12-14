f3c309e68724e706dad94cdf2c18d8bc7aff8674
vec <vec@moebaka.com>
2016-12-14 20:34:15 +0800

    数字模式

--- test/core.js
+++ test/core.js
@@ -24,11 +24,11 @@ describe('字符串模式', () => {
 
   it('未出现赋值语句', () => {
     try { suc.parse('propertyproperty'); }
-    catch (e) { e.code.should.equal(-1); }
+    catch (e) { e.code.should.equal(1); }
   });
   it('无属性名', () => {
     try { suc.parse('>this is String'); }
-    catch (e) { e.code.should.equal(-2); }
+    catch (e) { e.code.should.equal(2); }
   });
   it('字符类型', () => {
     const strTest = (property, value, checkProperty = property, checkValue = value) => {
@@ -44,5 +44,40 @@ describe('字符串模式', () => {
     strTest('   prop\t ', 'value', 'prop');
 
     strTest('ppp>>>', 'values', 'ppp', '>>>values');
+  });
+});
+
+describe('数字模式', () => {
+  const suc = new Suc;
+
+  it('未闭合的属性括号', () => {
+    try { suc.parse('[ppp 5838') }
+    catch (e) { e.code.should.equal(10) }
+  });
+
+  it('无属性名', () => {
+    try { suc.parse('[] 999') }
+    catch (e) { e.code.should.equal(11) }
+  })
+
+  it('数字的识别', () => {
+    let obj = suc.parse('[pppname]  385993');
+    obj.should.has.property('pppname');
+    obj.pppname.should.equal(385993);
+  });
+
+  it('非法数字', () => {
+    try { suc.parse('[pppname] 838 858'); }
+    catch (e) { e.code.should.equal(20) }
+  });
+
+  it('布尔型的识别', () => {
+    let obj = suc.parse('[name] true');
+    obj.should.has.property('name');
+    obj.name.should.equal(true);
+  });
+  it('非法布尔型', () => {
+    try { let obj = suc.parse('[name] ture'); }
+    catch (e) { e.code.should.equal(20) }
   });
 })
